fdca4600446f5a5e99161db1278a26641cc23e44
Philipp Kursawe <phil.kursawe@gmail.com>
2010-02-22 16:46:55 +0100

    Changed readme, added license

--- file.monitor.osgi.example.event/src/file/monitor/osgi/example/event/internal/FileEventHandler.java
+++ file.monitor.osgi.example.event/src/file/monitor/osgi/example/event/internal/FileEventHandler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Philipp Kursawe.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Philipp Kursawe (phil.kursawe@gmail.com) - initial API and implementation
+ ******************************************************************************/
 package file.monitor.osgi.example.event.internal;
 
 import java.util.concurrent.atomic.AtomicReference;
